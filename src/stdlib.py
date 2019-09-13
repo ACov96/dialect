a9f6625e50f51ff23d9a15c7e966e51a3d24ac29
Alexander Covington <covinga@honeydew.reshall.wwu.edu>
2019-09-13 08:56:56 -0700

    Object printing finished

--- src/stdlib.py
+++ src/stdlib.py
@@ -12,22 +12,24 @@ def _print(ctx, args):
         elif isinstance(x, str):
             return '"{}"'.format(x)
         elif isinstance(x, dict):
-            print (x)
-            return '\{{}\}'.format(', '.join(['{} : {}'.format(key, val) for key, val in x]))
+            key_vals = ['{} : {}'.format(key, x[key]) for key in x]
+            return '{{ {} }}'.format(', '.join(key_vals))
         return x
 
     def minify_list(l):
         l = deepcopy(l)
-        mini = [minify_list(el[1]) if el[0] == 'list' else format_str(el[1]) for el in l]
+        mini = [minify_list(el[1]) if el[0] == 'list' else el for el in l]
+        mini = [minify_dict(el[1]) if el[0] == 'object' else el for el in mini]
+        mini = [format_str(el[1]) if isinstance(el, tuple) else el for el in mini]
         return '[{}]'.format(', '.join(mini))
 
     def minify_dict(d):
         d = deepcopy(d)
         for key in d:
             if d[key][0] == 'object':
-                d[key] = minify_dict(d[key])
+                d[key] = minify_dict(d[key][1])
             elif d[key][0] == 'list':
-                d[key] = minify_list(d[key])
+                d[key] = minify_list(d[key][1])
             elif d[key][0] == 'number' and d[key][1].is_integer():
                 d[key] = int(d[key][1])
             else:
@@ -36,10 +38,9 @@ def _print(ctx, args):
                 
 
     evaluated_args = [eval_expr(ctx, arg)[1] for arg in args]
-    evaluated_args = [minify_list(arg) if isinstance(arg, list) else arg for arg in evaluated_args]
-    evaluated_args = [minify_dict(arg) if isinstance(arg, dict) else arg for arg in evaluated_args]
-    evaluated_args = [format_str(arg) for arg in evaluated_args]
-    # print(evaluated_args)
+    evaluated_args = [(minify_list(arg),) if isinstance(arg, list) else arg for arg in evaluated_args]
+    evaluated_args = [(minify_dict(arg),) if isinstance(arg, dict) else arg for arg in evaluated_args]
+    evaluated_args = [format_str(arg) if not isinstance(arg, tuple) else arg[0] for arg in evaluated_args]
     print(' '.join(evaluated_args))
         
 STDLIB = {
--- src/util.py
+++ src/util.py
@@ -1,5 +1,4 @@
 def make_obj(tuples):
-    print(tuples)
     obj = {}
     for key, val in tuples:
         obj[key] = val
