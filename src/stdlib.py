ac158465c64aea6b1110f265b16fc7f11ca4cbe0
Alex Covington <alex@covington.tech>
2019-09-12 17:19:06 -0700

    Better formatting of primitives when printing

--- src/stdlib.py
+++ src/stdlib.py
@@ -3,12 +3,21 @@ from context import Context
 def _print(ctx, args):
     from engine import eval_expr
 
+    def format_str(x):
+        if isinstance(x, float) and x.is_integer():
+            return str(int(x))
+        elif isinstance(x, float):
+            return str(x)
+        elif isinstance(x, str):
+            return '"{}"'.format(x)
+        return x
+
     def minify_list(l):
-        mini = [minify_list(el[1]) if el[0] == 'list' else str(el[1]) for el in l]
+        mini = [minify_list(el[1]) if el[0] == 'list' else format_str(el[1]) for el in l]
         return '[{}]'.format(', '.join(mini))
 
     evaluated_args = [eval_expr(ctx, arg)[1] for arg in args]
-    evaluated_args = [minify_list(arg) if isinstance(arg, list) else str(arg) for arg in evaluated_args]
+    evaluated_args = [minify_list(arg) if isinstance(arg, list) else format_str(arg) for arg in evaluated_args]
     print(' '.join(evaluated_args))
         
 STDLIB = {
