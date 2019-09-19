80796dfe25e1186b89e81938b4ea385396756f0a
Alex Covington <alex@covington.tech>
2019-09-19 13:40:40 -0700

    Restructuring

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt', 'r') as f:
+    requirements = f.readlines()
+
+setup(
+    name = 'dialect',
+    version = '0.1.3',
+    author = 'Alex Covington',
+    author_email = 'alex@covington.tech',
+    url = 'https://github.com/ACov96/dialect/tree/master',
+    packages = find_packages(),
+    license = 'MIT',
+    entry_points = {
+        'console_scripts': [
+            'dialect = dialect.main:main',
+        ],
+    },
+    install_requires = requirements,
+)
